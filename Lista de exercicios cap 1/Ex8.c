8a2517e1e091c7de12e2bee15326ffa2eef5ee13
JhorgeAraujo <jaa3@cesar.school>
2026-08-19 15:47:42 -0300

    Segunda bateria de exercicios

---
+++ Lista de exercicios cap 1/Ex8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+printf("\n\t\"Primeiro programa\"");
+getchar(); //utilizo o linux por isso esse comando. ele tem a memsa funçao do PAUSE, interromper o programa ate o usuario apertrar enter.
+return 0;
+}
+
+/*o comando imprime a frase "Primeiro programa" (as aspas sao inseridas no printo pelo comando \"),
+ mas antes o terminal pula uma linha e da uma tabulação */
